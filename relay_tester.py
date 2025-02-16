63b37279d9f3e577c3b142e517c64b88c69ac552
colehanan1 <c.b.hanan@wustl.edu>
2025-02-16 17:50:24 -0600

    cool widgets

--- relay_tester.py
+++ relay_tester.py
@@ -82,7 +82,7 @@ app.bind("<Key>", on_key)
 # Load images with Pillow, then wrap in CTkImage.
 try:
     meshalyzer_image_pil = Image.open("MeshAlyzer.jpeg")
-    stl_image_pil        = Image.open("stl_logo.png")
+    stl_image_pil        = Image.open("lakelab.jpg")
 except FileNotFoundError:
     # If you don't have the actual files or they're named differently,
     # adjust the paths or handle errors as needed.
