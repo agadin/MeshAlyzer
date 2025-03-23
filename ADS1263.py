679ea58da301e94d747f1728b3fbcf1750776a44
agadin <a.gadin@wustl.edu>
2025-03-22 21:14:08 -0500

    feat(ADS1263.py): integrate spidev for SPI communication and enhance GPIO handling

--- ADS1263.py
+++ ADS1263.py
@@ -1,4 +1,6 @@
+#!/usr/bin/python3
 import lgpio
+import spidev
 import time
 
 # ---------------------------------------------------------------------------
@@ -11,141 +13,141 @@ LOW = 0
 
 # gain
 ADS1263_GAIN = {
-    'ADS1263_GAIN_1' : 0,   # GAIN   1
-    'ADS1263_GAIN_2' : 1,   # GAIN   2
-    'ADS1263_GAIN_4' : 2,   # GAIN   4
-    'ADS1263_GAIN_8' : 3,   # GAIN   8
-    'ADS1263_GAIN_16' : 4,  # GAIN  16
-    'ADS1263_GAIN_32' : 5,  # GAIN  32
-    'ADS1263_GAIN_64' : 6,  # GAIN  64
+    'ADS1263_GAIN_1': 0,  # GAIN   1
+    'ADS1263_GAIN_2': 1,  # GAIN   2
+    'ADS1263_GAIN_4': 2,  # GAIN   4
+    'ADS1263_GAIN_8': 3,  # GAIN   8
+    'ADS1263_GAIN_16': 4,  # GAIN  16
+    'ADS1263_GAIN_32': 5,  # GAIN  32
+    'ADS1263_GAIN_64': 6,  # GAIN  64
 }
 # ADC2 gain
 ADS1263_ADC2_GAIN = {
-    'ADS1263_ADC2_GAIN_1'   : 0,
-    'ADS1263_ADC2_GAIN_2'   : 1,
-    'ADS1263_ADC2_GAIN_4'   : 2,
-    'ADS1263_ADC2_GAIN_8'   : 3,
-    'ADS1263_ADC2_GAIN_16'  : 4,
-    'ADS1263_ADC2_GAIN_32'  : 5,
-    'ADS1263_ADC2_GAIN_64'  : 6,
-    'ADS1263_ADC2_GAIN_128' : 7,
+    'ADS1263_ADC2_GAIN_1': 0,
+    'ADS1263_ADC2_GAIN_2': 1,
+    'ADS1263_ADC2_GAIN_4': 2,
+    'ADS1263_ADC2_GAIN_8': 3,
+    'ADS1263_ADC2_GAIN_16': 4,
+    'ADS1263_ADC2_GAIN_32': 5,
+    'ADS1263_ADC2_GAIN_64': 6,
+    'ADS1263_ADC2_GAIN_128': 7,
 }
 # data rate
 ADS1263_DRATE = {
-    'ADS1263_38400SPS'  : 0xF,
-    'ADS1263_19200SPS'  : 0xE,
-    'ADS1263_14400SPS'  : 0xD,
-    'ADS1263_7200SPS'   : 0xC,
-    'ADS1263_4800SPS'   : 0xB,
-    'ADS1263_2400SPS'   : 0xA,
-    'ADS1263_1200SPS'   : 0x9,
-    'ADS1263_400SPS'    : 0x8,
-    'ADS1263_100SPS'    : 0x7,
-    'ADS1263_60SPS'     : 0x6,
-    'ADS1263_50SPS'     : 0x5,
-    'ADS1263_20SPS'     : 0x4,
-    'ADS1263_16d6SPS'   : 0x3,
-    'ADS1263_10SPS'     : 0x2,
-    'ADS1263_5SPS'      : 0x1,
-    'ADS1263_2d5SPS'    : 0x0,
+    'ADS1263_38400SPS': 0xF,
+    'ADS1263_19200SPS': 0xE,
+    'ADS1263_14400SPS': 0xD,
+    'ADS1263_7200SPS': 0xC,
+    'ADS1263_4800SPS': 0xB,
+    'ADS1263_2400SPS': 0xA,
+    'ADS1263_1200SPS': 0x9,
+    'ADS1263_400SPS': 0x8,
+    'ADS1263_100SPS': 0x7,
+    'ADS1263_60SPS': 0x6,
+    'ADS1263_50SPS': 0x5,
+    'ADS1263_20SPS': 0x4,
+    'ADS1263_16d6SPS': 0x3,
+    'ADS1263_10SPS': 0x2,
+    'ADS1263_5SPS': 0x1,
+    'ADS1263_2d5SPS': 0x0,
 }
 # ADC2 data rate
 ADS1263_ADC2_DRATE = {
-    'ADS1263_ADC2_10SPS'    : 0,
-    'ADS1263_ADC2_100SPS'   : 1,
-    'ADS1263_ADC2_400SPS'   : 2,
-    'ADS1263_ADC2_800SPS'   : 3,
+    'ADS1263_ADC2_10SPS': 0,
+    'ADS1263_ADC2_100SPS': 1,
+    'ADS1263_ADC2_400SPS': 2,
+    'ADS1263_ADC2_800SPS': 3,
 }
 # Delay time
 ADS1263_DELAY = {
-    'ADS1263_DELAY_0s'      : 0,
-    'ADS1263_DELAY_8d7us'   : 1,
-    'ADS1263_DELAY_17us'    : 2,
-    'ADS1263_DELAY_35us'    : 3,
-    'ADS1263_DELAY_169us'   : 4,
-    'ADS1263_DELAY_139us'   : 5,
-    'ADS1263_DELAY_278us'   : 6,
-    'ADS1263_DELAY_555us'   : 7,
-    'ADS1263_DELAY_1d1ms'   : 8,
-    'ADS1263_DELAY_2d2ms'   : 9,
-    'ADS1263_DELAY_4d4ms'   : 10,
-    'ADS1263_DELAY_8d8ms'   : 11,
+    'ADS1263_DELAY_0s': 0,
+    'ADS1263_DELAY_8d7us': 1,
+    'ADS1263_DELAY_17us': 2,
+    'ADS1263_DELAY_35us': 3,
+    'ADS1263_DELAY_169us': 4,
+    'ADS1263_DELAY_139us': 5,
+    'ADS1263_DELAY_278us': 6,
+    'ADS1263_DELAY_555us': 7,
+    'ADS1263_DELAY_1d1ms': 8,
+    'ADS1263_DELAY_2d2ms': 9,
+    'ADS1263_DELAY_4d4ms': 10,
+    'ADS1263_DELAY_8d8ms': 11,
 }
 # DAC out volt
 ADS1263_DAC_VOLT = {
-    'ADS1263_DAC_VLOT_4_5'      : 0b01001,      #4.5V
-    'ADS1263_DAC_VLOT_3_5'      : 0b01000,
-    'ADS1263_DAC_VLOT_3'        : 0b00111,
-    'ADS1263_DAC_VLOT_2_75'     : 0b00110,
-    'ADS1263_DAC_VLOT_2_625'    : 0b00101,
-    'ADS1263_DAC_VLOT_2_5625'   : 0b00100,
-    'ADS1263_DAC_VLOT_2_53125'  : 0b00011,
-    'ADS1263_DAC_VLOT_2_515625' : 0b00010,
+    'ADS1263_DAC_VLOT_4_5': 0b01001,  # 4.5V
+    'ADS1263_DAC_VLOT_3_5': 0b01000,
+    'ADS1263_DAC_VLOT_3': 0b00111,
+    'ADS1263_DAC_VLOT_2_75': 0b00110,
+    'ADS1263_DAC_VLOT_2_625': 0b00101,
+    'ADS1263_DAC_VLOT_2_5625': 0b00100,
+    'ADS1263_DAC_VLOT_2_53125': 0b00011,
+    'ADS1263_DAC_VLOT_2_515625': 0b00010,
     'ADS1263_DAC_VLOT_2_5078125': 0b00001,
-    'ADS1263_DAC_VLOT_2_5'      : 0b00000,
+    'ADS1263_DAC_VLOT_2_5': 0b00000,
     'ADS1263_DAC_VLOT_2_4921875': 0b10001,
-    'ADS1263_DAC_VLOT_2_484375' : 0b10010,
-    'ADS1263_DAC_VLOT_2_46875'  : 0b10011,
-    'ADS1263_DAC_VLOT_2_4375'   : 0b10100,
-    'ADS1263_DAC_VLOT_2_375'    : 0b10101,
-    'ADS1263_DAC_VLOT_2_25'     : 0b10110,
-    'ADS1263_DAC_VLOT_2'        : 0b10111,
-    'ADS1263_DAC_VLOT_1_5'      : 0b11000,
-    'ADS1263_DAC_VLOT_0_5'      : 0b11001,
+    'ADS1263_DAC_VLOT_2_484375': 0b10010,
+    'ADS1263_DAC_VLOT_2_46875': 0b10011,
+    'ADS1263_DAC_VLOT_2_4375': 0b10100,
+    'ADS1263_DAC_VLOT_2_375': 0b10101,
+    'ADS1263_DAC_VLOT_2_25': 0b10110,
+    'ADS1263_DAC_VLOT_2': 0b10111,
+    'ADS1263_DAC_VLOT_1_5': 0b11000,
+    'ADS1263_DAC_VLOT_0_5': 0b11001,
 }
 # registration definition
 ADS1263_REG = {
-    'REG_ID'        : 0,
-    'REG_POWER'     : 1,
-    'REG_INTERFACE' : 2,
-    'REG_MODE0'     : 3,
-    'REG_MODE1'     : 4,
-    'REG_MODE2'     : 5,
-    'REG_INPMUX'    : 6,
-    'REG_OFCAL0'    : 7,
-    'REG_OFCAL1'    : 8,
-    'REG_OFCAL2'    : 9,
-    'REG_FSCAL0'    : 10,
-    'REG_FSCAL1'    : 11,
-    'REG_FSCAL2'    : 12,
-    'REG_IDACMUX'   : 13,
-    'REG_IDACMAG'   : 14,
-    'REG_REFMUX'    : 15,
-    'REG_TDACP'     : 16,
-    'REG_TDACN'     : 17,
-    'REG_GPIOCON'   : 18,
-    'REG_GPIODIR'   : 19,
-    'REG_GPIODAT'   : 20,
-    'REG_ADC2CFG'   : 21,
-    'REG_ADC2MUX'   : 22,
-    'REG_ADC2OFC0'  : 23,
-    'REG_ADC2OFC1'  : 24,
-    'REG_ADC2FSC0'  : 25,
-    'REG_ADC2FSC1'  : 26,
+    'REG_ID': 0,
+    'REG_POWER': 1,
+    'REG_INTERFACE': 2,
+    'REG_MODE0': 3,
+    'REG_MODE1': 4,
+    'REG_MODE2': 5,
+    'REG_INPMUX': 6,
+    'REG_OFCAL0': 7,
+    'REG_OFCAL1': 8,
+    'REG_OFCAL2': 9,
+    'REG_FSCAL0': 10,
+    'REG_FSCAL1': 11,
+    'REG_FSCAL2': 12,
+    'REG_IDACMUX': 13,
+    'REG_IDACMAG': 14,
+    'REG_REFMUX': 15,
+    'REG_TDACP': 16,
+    'REG_TDACN': 17,
+    'REG_GPIOCON': 18,
+    'REG_GPIODIR': 19,
+    'REG_GPIODAT': 20,
+    'REG_ADC2CFG': 21,
+    'REG_ADC2MUX': 22,
+    'REG_ADC2OFC0': 23,
+    'REG_ADC2OFC1': 24,
+    'REG_ADC2FSC0': 25,
+    'REG_ADC2FSC1': 26,
 }
 # command definitions
 ADS1263_CMD = {
-    'CMD_RESET'     : 0x06,
-    'CMD_START1'    : 0x08,
-    'CMD_STOP1'     : 0x0A,
-    'CMD_START2'    : 0x0C,
-    'CMD_STOP2'     : 0x0E,
-    'CMD_RDATA1'    : 0x12,
-    'CMD_RDATA2'    : 0x14,
-    'CMD_SYOCAL1'   : 0x16,
-    'CMD_SYGCAL1'   : 0x17,
-    'CMD_SFOCAL1'   : 0x19,
-    'CMD_SYOCAL2'   : 0x1B,
-    'CMD_SYGCAL2'   : 0x1C,
-    'CMD_SFOCAL2'   : 0x1E,
-    'CMD_RREG'      : 0x20,
-    'CMD_RREG2'     : 0x00,
-    'CMD_WREG'      : 0x40,
-    'CMD_WREG2'     : 0x00,
+    'CMD_RESET': 0x06,
+    'CMD_START1': 0x08,
+    'CMD_STOP1': 0x0A,
+    'CMD_START2': 0x0C,
+    'CMD_STOP2': 0x0E,
+    'CMD_RDATA1': 0x12,
+    'CMD_RDATA2': 0x14,
+    'CMD_SYOCAL1': 0x16,
+    'CMD_SYGCAL1': 0x17,
+    'CMD_SFOCAL1': 0x19,
+    'CMD_SYOCAL2': 0x1B,
+    'CMD_SYGCAL2': 0x1C,
+    'CMD_SFOCAL2': 0x1E,
+    'CMD_RREG': 0x20,
+    'CMD_RREG2': 0x00,
+    'CMD_WREG': 0x40,
+    'CMD_WREG2': 0x00,
 }
 
 # ---------------------------------------------------------------------------
-# Minimal config module implemented with lgpio
+# Minimal config module implemented with lgpio and spidev for SPI
 #
 # This layer provides:
 #   module_init() and module_exit() for setting up/closing the SPI and GPIO chip,
@@ -153,52 +155,65 @@ ADS1263_CMD = {
 #   delay_ms(ms),
 #   spi_writebyte(data) and spi_readbytes(n)
 #
-# Adjust the lgpio API calls below as needed for your environment.
+# Adjust the API calls below as needed for your environment.
 #
 
 # Global handles
 _chip = None
-_spi_handle = None
+_spi = None  # spidev instance for SPI transfers
 _gpio_wrapper = None
 
+
 def module_init():
-    global _chip, _spi_handle, _gpio_wrapper
-    # Open GPIO chip 0
+    global _chip, _spi, _gpio_wrapper
+    # Open GPIO chip 0 using lgpio
     _chip = lgpio.gpiochip_open(0)
-    # Create our GPIO wrapper using the chip handle
     _gpio_wrapper = LGPIOWrapper(_chip)
-    # Open SPI channel 0; try channel 0 or 1 as needed.
-    _spi_handle = lgpio.spi_open(_chip, 0, 250000, 0)
-    if isinstance(_spi_handle, int) and _spi_handle < 0:
-        raise RuntimeError("spi_open failed: can not open SPI device")
+
+    # Open SPI device using spidev (bus 0, CS 0)
+    _spi = spidev.SpiDev()
+    _spi.open(0, 0)
+    _spi.max_speed_hz = 250000  # Adjust speed as needed
+    _spi.mode = 0
     return 0
 
+
 def module_exit():
-    global _chip, _spi_handle, _gpio_wrapper
+    global _chip, _spi, _gpio_wrapper
     try:
-        if _spi_handle is not None and _spi_handle >= 0:
-            lgpio.spi_close(_spi_handle)
+        if _spi is not None:
+            _spi.close()
     except Exception as e:
         print("Error closing SPI:", e)
     _gpio_wrapper.cleanup()
     lgpio.gpiochip_close(_chip)
 
+
+def digital_write(pin, value):
+    global _gpio_wrapper
+    _gpio_wrapper.digital_write(pin, value)
+
+
 def digital_read(pin):
     global _gpio_wrapper
     return _gpio_wrapper.digital_read(pin)
 
+
 def delay_ms(ms):
     time.sleep(ms / 1000.0)
 
+
 def spi_writebyte(data):
-    global _spi_handle
-    # data is a list of integers; convert to bytes and write via SPI
-    lgpio.spi_write(_spi_handle, bytes(data))
+    global _spi
+    # Use spidev's full-duplex transfer; ignore returned data.
+    _spi.xfer2(data)
+
 
 def spi_readbytes(n):
-    global _spi_handle
-    # Read n bytes and return as a list of integers
-    return list(lgpio.spi_read(_spi_handle, n))
+    global _spi
+    # To read n bytes, send n dummy bytes (0x00)
+    return _spi.xfer2([0] * n)
+
 
 # ---------------------------------------------------------------------------
 # LGPIOWrapper class for digital I/O
@@ -211,38 +226,43 @@ class LGPIOWrapper:
         self.chip = chip
         self.out_lines = {}
         self.in_lines = {}
+
     def setup_out(self, pin):
         # Request an output line; adjust parameters as needed.
         handle = lgpio.gpio_request_line(self.chip, [pin], "ADS1263_out", 0, 0)
         self.out_lines[pin] = handle
+
     def setup_in(self, pin):
         # Request an input line; adjust parameters as needed.
         handle = lgpio.gpio_request_line(self.chip, [pin], "ADS1263_in", 0, 1)
         self.in_lines[pin] = handle
+
     def digital_write(self, pin, value):
         if pin not in self.out_lines:
             self.setup_out(pin)
         lgpio.gpio_set_line_value(self.out_lines[pin], value)
+
     def digital_read(self, pin):
         if pin not in self.in_lines:
             self.setup_in(pin)
         return lgpio.gpio_get_line_value(self.in_lines[pin])
+
     def cleanup(self):
         for handle in self.out_lines.values():
             lgpio.gpio_release_line(handle)
         for handle in self.in_lines.values():
             lgpio.gpio_release_line(handle)
 
+
 # ---------------------------------------------------------------------------
-# ADS1263 Driver Class (porting from RPi.GPIO to use our lgpio-based config)
+# ADS1263 Driver Class (porting from RPi.GPIO to use our lgpio-based config and spidev for SPI)
 #
 class ADS1263:
     def __init__(self):
-        # These pin numbers should be defined in your configuration.
-        # For example, you might assign:
-        self.rst_pin = 5    # Replace with your actual reset pin number
-        self.cs_pin = 6     # Replace with your actual chip-select pin number
-        self.drdy_pin = 13  # Replace with your actual DRDY pin number
+        # Define your pin numbers as needed:
+        self.rst_pin = 5  # Reset pin
+        self.cs_pin = 6  # Chip-select pin
+        self.drdy_pin = 13  # Data ready pin
         self.ScanMode = 1
 
     # Hardware reset
@@ -333,7 +353,7 @@ class ADS1263:
 
     # Configure ADC2 parameters: gain and data rate
     def ADS1263_ConfigADC2(self, gain, drate):
-        ADC2CFG = 0x20  # 0x20: use VAVDD/VAVSS as reference
+        ADC2CFG = 0x20  # Use VAVDD/VAVSS as reference
         ADC2CFG |= (drate << 6) | gain
         self.ADS1263_WriteReg(ADS1263_REG['REG_ADC2CFG'], ADC2CFG)
         if self.ADS1263_ReadData(ADS1263_REG['REG_ADC2CFG'])[0] == ADC2CFG:
@@ -369,15 +389,15 @@ class ADS1263:
     # Set ADC1 differential channel
     def ADS1263_SetDiffChannal(self, Channal):
         if Channal == 0:
-            INPMUX = (0 << 4) | 1   # AIN0-AIN1
+            INPMUX = (0 << 4) | 1  # AIN0-AIN1
         elif Channal == 1:
-            INPMUX = (2 << 4) | 3   # AIN2-AIN3
+            INPMUX = (2 << 4) | 3  # AIN2-AIN3
         elif Channal == 2:
-            INPMUX = (4 << 4) | 5   # AIN4-AIN5
+            INPMUX = (4 << 4) | 5  # AIN4-AIN5
         elif Channal == 3:
-            INPMUX = (6 << 4) | 7   # AIN6-AIN7
+            INPMUX = (6 << 4) | 7  # AIN6-AIN7
         elif Channal == 4:
-            INPMUX = (8 << 4) | 9   # AIN8-AIN9
+            INPMUX = (8 << 4) | 9  # AIN8-AIN9
         self.ADS1263_WriteReg(ADS1263_REG['REG_INPMUX'], INPMUX)
         if self.ADS1263_ReadData(ADS1263_REG['REG_INPMUX'])[0] != INPMUX:
             print("REG_INPMUX unsuccess")
@@ -439,9 +459,9 @@ class ADS1263:
                 break
         buf = spi_readbytes(5)
         digital_write(self.cs_pin, HIGH)
-        read  = ((buf[0] << 24) & 0xff000000)
+        read = ((buf[0] << 24) & 0xff000000)
         read |= ((buf[1] << 16) & 0xff0000)
-        read |= ((buf[2] << 8)  & 0xff00)
+        read |= ((buf[2] << 8) & 0xff00)
         read |= (buf[3] & 0xff)
         CRC = buf[4]
         if self.ADS1263_CheckSum(read, CRC) != 0:
@@ -459,7 +479,7 @@ class ADS1263:
         buf = spi_readbytes(5)
         digital_write(self.cs_pin, HIGH)
         read |= ((buf[0] << 16) & 0xff0000)
-        read |= ((buf[1] << 8)  & 0xff00)
+        read |= ((buf[1] << 8) & 0xff00)
         read |= (buf[2] & 0xff)
         CRC = buf[4]
         if self.ADS1263_CheckSum(read, CRC) != 0:
