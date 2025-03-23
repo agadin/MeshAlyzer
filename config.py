bf94da7fa610165e292f07d1ba207576ff19c8a9
agadin <a.gadin@wustl.edu>
2025-03-22 21:35:33 -0500

    feat(config.py): update hardware interface to use lgpio for Raspberry Pi

--- config.py
+++ config.py
@@ -1,94 +1,89 @@
-# /*****************************************************************************
-# * | File        :	  config.py
-# * | Author      :   Waveshare team
-# * | Function    :   Hardware underlying interface
-# * | Info        :
-# *----------------
-# * | This version:   V1.0
-# * | Date        :   2020-12-12
-# * | Info        :   
-# ******************************************************************************/
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documnetation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to  whom the Software is
-# furished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in
-# all copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS OR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-# THE SOFTWARE.
-#
+#!/usr/bin/env python3
+"""
+/*****************************************************************************
+ * | File        :   config.py
+ * | Author      :   Waveshare team (modified)
+ * | Function    :   Hardware underlying interface using lgpio for Raspberry Pi
+ * | Info        :
+ *----------------
+ * | This version:   V1.0 (modified)
+ * | Date        :   2020-12-12 (original), updated for lgpio
+ * | Info        :
+ ******************************************************************************
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction...
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND...
+ *
+ *****************************************************************************/
+"""
+
 import os
 import sys
 import time
+import spidev
+import lgpio  # Using lgpio instead of RPi.GPIO
+
 
 class RaspberryPi:
     # Pin definition
-    RST_PIN     = 18
-    CS_PIN      = 22
-    DRDY_PIN    = 17
+    RST_PIN = 18
+    CS_PIN = 22
+    DRDY_PIN = 17
 
     def __init__(self):
-    # SPI device, bus = 0, device = 0
-        import spidev
-        import RPi.GPIO
-        
-        self.GPIO = RPi.GPIO
         self.SPI = spidev.SpiDev(0, 0)
+        self.chip_handle = None
 
     def digital_write(self, pin, value):
-        self.GPIO.output(pin, value)
+        lgpio.gpio_write(self.chip_handle, pin, value)
 
     def digital_read(self, pin):
-        return self.GPIO.input(pin)
+        return lgpio.gpio_read(self.chip_handle, pin)
 
     def delay_ms(self, delaytime):
         time.sleep(delaytime / 1000.0)
 
     def spi_writebyte(self, data):
         self.SPI.writebytes(data)
-        
-    def spi_readbytes(self, reg):
-        return self.SPI.readbytes(reg)
-        
+
+    def spi_readbytes(self, n):
+        return self.SPI.readbytes(n)
+
     def module_init(self):
-        self.GPIO.setmode(self.GPIO.BCM)
-        self.GPIO.setwarnings(False)
-        self.GPIO.setup(self.RST_PIN, self.GPIO.OUT)
-        self.GPIO.setup(self.CS_PIN, self.GPIO.OUT)
-        
-        self.GPIO.setup(self.DRDY_PIN, self.GPIO.IN, pull_up_down=self.GPIO.PUD_UP)
+        # Initialize SPI
         self.SPI.max_speed_hz = 2000000
         self.SPI.mode = 0b01
-        return 0;
+
+        # Initialize lgpio: open GPIO chip 0
+        self.chip_handle = lgpio.gpiochip_open(0)
+        # Claim outputs for RST and CS with initial values: RST LOW, CS HIGH
+        lgpio.gpio_claim_output(self.chip_handle, self.RST_PIN, 0)
+        lgpio.gpio_claim_output(self.chip_handle, self.CS_PIN, 1)
+        # Claim input for DRDY
+        lgpio.gpio_claim_input(self.chip_handle, self.DRDY_PIN)
+        return 0
 
     def module_exit(self):
+        # Set pins to a safe state and close resources
+        lgpio.gpio_write(self.chip_handle, self.RST_PIN, 0)
+        lgpio.gpio_write(self.chip_handle, self.CS_PIN, 0)
         self.SPI.close()
-        self.GPIO.output(self.RST_PIN, 0)
-        self.GPIO.output(self.CS_PIN, 0)
-        self.GPIO.cleanup()
-        
-        
+        lgpio.gpiochip_close(self.chip_handle)
+
+
 class JetsonNano:
     # Pin definition
-    RST_PIN         = 18
-    CS_PIN          = 22
-    DRDY_PIN        = 17
+    RST_PIN = 18
+    CS_PIN = 22
+    DRDY_PIN = 17
 
     def __init__(self):
-        import spidev
         self.SPI = spidev.SpiDev(0, 0)
-        
-        import Jetson.GPIO
-        self.GPIO = Jetson.GPIO
+        import Jetson.GPIO as GPIO
+        self.GPIO = GPIO
 
     def digital_write(self, pin, value):
         self.GPIO.output(pin, value)
@@ -101,9 +96,9 @@ class JetsonNano:
 
     def spi_writebyte(self, data):
         self.SPI.writebytes(data)
-        
-    def spi_readbytes(self, reg):
-        return self.SPI.readbytes(reg)
+
+    def spi_readbytes(self, n):
+        return self.SPI.readbytes(n)
 
     def module_init(self):
         self.GPIO.setmode(self.GPIO.BCM)
@@ -118,12 +113,11 @@ class JetsonNano:
     def module_exit(self):
         self.SPI.close()
         self.GPIO.output(self.RST_PIN, 0)
-
         self.GPIO.cleanup()
-        
+
+
 hostname = os.popen("uname -n").read().strip()
-        
-#if os.path.exists('/sys/bus/platform/drivers/gpiomem-bcm2835'):
+
 if hostname == "raspberrypi":
     implementation = RaspberryPi()
 else:
